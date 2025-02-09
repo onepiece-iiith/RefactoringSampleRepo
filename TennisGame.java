f2830bf506f78acd759956453b67035b3e46a440
onepieceIsReal <onepiece.iiith@gmail.com>
2025-02-09 16:59:46 +0530

    Refactored code to address design smells

--- TennisGame.java
+++ TennisGame.java
@@ -17,69 +17,81 @@ public class TennisGame1 implements TennisGame {
     }
 
     public String getScore() {
-        String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            score = getScoreForEqualScores(m_score1);
-        }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            score = getScoreForAdvantageOrWin(m_score1, m_score2);
-        }
-        else
-        {
-            score = getScoreForRegularPlay(m_score1, m_score2);
-        }
-        return score;
+        return new ScoreDisplay(m_score1, m_score2).getScore();
     }
+    
+    private class ScoreDisplay{
+        private int m_score1;
+        private int m_score2;
+        
+        public ScoreDisplay(int score1, int score2){
+            this.m_score1 = score1;
+            this.m_score2 = score2;
+        }
+        
+        public String getScore(){
+            String score = "";
+            if (m_score1==m_score2)
+            {
+                score = getScoreForEqualScores(m_score1);
+            }
+            else if (m_score1>=4 || m_score2>=4)
+            {
+                score = getScoreForAdvantageOrWin(m_score1, m_score2);
+            }
+            else
+            {
+                score = getScoreForRegularPlay(m_score1, m_score2);
+            }
+            return score;
+        }
 
-    private String getScoreForEqualScores(int score) {
-        switch (score) {
-            case 0:
-                return "Love-All";
-            case 1:
-                return "Fifteen-All";
-            case 2:
-                return "Thirty-All";
-            default:
-                return "Deuce";
+        private String getScoreForEqualScores(int score) {
+            switch (score) {
+                case 0:
+                    return "Love-All";
+                case 1:
+                    return "Fifteen-All";
+                case 2:
+                    return "Thirty-All";
+                default:
+                    return "Deuce";
+            }
+        }
+
+        private String getScoreForAdvantageOrWin(int score1, int score2) {
+            int minusResult = score1-score2;
+            if (minusResult==1) return "Advantage player1";
+            else if (minusResult ==-1) return "Advantage player2";
+            else if (minusResult>=2) return "Win for player1";
+            else return "Win for player2";
+        }
+
+        private String getScoreForRegularPlay(int score1, int score2) {
+            String score = "";
+            for (int i=1; i<3; i++)
+            {
+                if (i==1) tempScore = score1;
+                else { score+="-"; tempScore = score2;}
+                score += getScoreDescription(tempScore);
+            }
+            return score;
+        }
+
+        private String getScoreDescription(int tempScore) {
+            switch(tempScore)
+            {
+                case 0:
+                    return "Love";
+                case 1:
+                    return "Fifteen";
+                case 2:
+                    return "Thirty";
+                case 3:
+                    return "Forty";
+                default:
+                    return "";
+            }
         }
     }
-
-    private String getScoreForAdvantageOrWin(int score1, int score2) {
-        int minusResult = score1-score2;
-        if (minusResult==1) return "Advantage player1";
-        else if (minusResult ==-1) return "Advantage player2";
-        else if (minusResult>=2) return "Win for player1";
-        else return "Win for player2";
-    }
-
-    private String getScoreForRegularPlay(int score1, int score2) {
-        String score = "";
-        for (int i=1; i<3; i++)
-        {
-            if (i==1) tempScore = score1;
-            else { score+="-"; tempScore = score2;}
-            score += getScoreDescription(tempScore);
-        }
-        return score;
-    }
-
-    private String getScoreDescription(int tempScore) {
-        switch(tempScore)
-        {
-            case 0:
-                return "Love";
-            case 1:
-                return "Fifteen";
-            case 2:
-                return "Thirty";
-            case 3:
-                return "Forty";
-            default:
-                return "";
-        }
-    }
-}    
-
+}
