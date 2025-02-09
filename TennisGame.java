d25ca8705598a83d8ca839c4d755fee693f84bab
onepieceIsReal <onepiece.iiith@gmail.com>
2025-02-09 18:09:22 +0530

    Refactored code for Switch Statement

--- TennisGame.java
+++ TennisGame.java
@@ -1,4 +1,3 @@
-
 public class TennisGame1 implements TennisGame {
     
     private int m_score1 = 0;
@@ -19,56 +18,59 @@ public class TennisGame1 implements TennisGame {
     }
 
     public String getScore() {
+        if (m_score1==m_score2) return getScoreForEqualScores();
+        if (m_score1>=4 || m_score2>=4) return getScoreForAdvantageOrWin();
+        return getScoreForNormalScores();
+    }
+
+    private String getScoreForEqualScores(){
+        String score = "";
+        switch (m_score1) {
+            case 0:
+                score = "Love-All";
+                break;
+            case 1:
+                score = "Fifteen-All";
+                break;
+            case 2:
+                score = "Thirty-All";
+                break;
+            default:
+                score = "Deuce";
+                break;
+        }
+        return score;
+    }
+
+    private String getScoreForAdvantageOrWin(){
+        String score = "";
+        int minusResult = m_score1-m_score2;
+        if (minusResult==1) score ="Advantage player1";
+        else if (minusResult ==-1) score ="Advantage player2";
+        else if (minusResult>=2) score = "Win for player1";
+        else score ="Win for player2";
+        return score;
+    }
+
+    private String getScoreForNormalScores(){
         String score = "";
         int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
+        for (int i=1; i<3; i++) {
+            if (i==1) tempScore = m_score1;
+            else { score+="-"; tempScore = m_score2;}
+            switch(tempScore) {
                 case 0:
-                        score = "Love-All";
+                    score+="Love";
                     break;
                 case 1:
-                        score = "Fifteen-All";
+                    score+="Fifteen";
                     break;
                 case 2:
-                        score = "Thirty-All";
+                    score+="Thirty";
                     break;
-                default:
-                        score = "Deuce";
+                case 3:
+                    score+="Forty";
                     break;
-                
-            }
-        }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
             }
         }
         return score;
