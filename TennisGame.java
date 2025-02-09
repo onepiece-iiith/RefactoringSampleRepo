125a77392766a3e76e3e5f3c20e06f82b705a859
onepieceIsReal <onepiece.iiith@gmail.com>
2025-02-09 19:29:02 +0530

    Refactor: Replace switch statements with polymorphism in TennisGame1

--- TennisGame.java
+++ TennisGame.java
@@ -1,6 +1,4 @@
-
 public class TennisGame1 implements TennisGame {
-    
     private int m_score1 = 0;
     private int m_score2 = 0;
     private String player1Name;
@@ -20,57 +18,67 @@ public class TennisGame1 implements TennisGame {
 
     public String getScore() {
         String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
+        int tempScore = 0;
+        if (m_score1 == m_score2) {
+            score = getScoreForEqualScores(m_score1);
+        } else if (m_score1 >= 4 || m_score2 >= 4) {
+            score = getScoreForAdvantageOrWin(m_score1, m_score2);
+        } else {
+            score = getScoreForNormalPlay(m_score1, m_score2);
         }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+        return score;
+    }
+
+    private String getScoreForEqualScores(int score) {
+        switch (score) {
+            case 0:
+                return "Love-All";
+            case 1:
+                return "Fifteen-All";
+            case 2:
+                return "Thirty-All";
+            default:
+                return "Deuce";
         }
+    }
+
+    private String getScoreForAdvantageOrWin(int score1, int score2) {
+        int minusResult = score1 - score2;
+        if (minusResult == 1)
+            return "Advantage player1";
+        else if (minusResult == -1)
+            return "Advantage player2";
+        else if (minusResult >= 2)
+            return "Win for player1";
         else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
+            return "Win for player2";
+    }
+
+    private String getScoreForNormalPlay(int score1, int score2) {
+        String score = "";
+        for (int i = 1; i < 3; i++) {
+            if (i == 1)
+                score = getScoreForSinglePlayer(score1);
+            else {
+                score += "-";
+                score += getScoreForSinglePlayer(score2);
             }
         }
         return score;
+    }
+
+    private String getScoreForSinglePlayer(int score) {
+        switch (score) {
+            case 0:
+                return "Love";
+            case 1:
+                return "Fifteen";
+            case 2:
+                return "Thirty";
+            case 3:
+                return "Forty";
+            default:
+                return "";
+        }
     }
 }
