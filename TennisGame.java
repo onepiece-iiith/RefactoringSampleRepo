2adb08bbd7b43d13f20d1630cde57344a0cd654f
onepieceIsReal <onepiece.iiith@gmail.com>
2025-02-09 19:34:53 +0530

    Refactor: Decompose TennisGame1 class to improve organization

--- TennisGame.java
+++ TennisGame.java
@@ -1,5 +1,4 @@
 public class TennisGame1 implements TennisGame {
-    
     private int m_score1 = 0;
     private int m_score2 = 0;
     private String player1Name;
@@ -18,44 +17,61 @@ public class TennisGame1 implements TennisGame {
     }
 
     public String getScore() {
-        String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            score = getEqualScore(m_score1);
-        }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            score = getScores(m_score1,m_score2);
-        }
-        return score;
+        return new ScoreCalculator(m_score1, m_score2).calculateScore();
     }
 
-    private String getEqualScore(int score) {
-        switch (score) {
-            case 0:
-                return "Love-All";
-            case 1:
-                return "Fifteen-All";
-            case 2:
-                return "Thirty-All";
-            default:
-                return "Deuce";
+    private class ScoreCalculator {
+        private int m_score1;
+        private int m_score2;
+
+        public ScoreCalculator(int m_score1, int m_score2) {
+            this.m_score1 = m_score1;
+            this.m_score2 = m_score2;
+        }
+
+        public String calculateScore() {
+            String score = "";
+            if (m_score1 == m_score2) {
+                score = getEqualScore(m_score1);
+            } else if (m_score1 >= 4 || m_score2 >= 4) {
+                score = calculateAdvantageOrWin();
+            } else {
+                score = getScores(m_score1, m_score2);
+            }
+            return score;
+        }
+
+        private String calculateAdvantageOrWin() {
+            int minusResult = m_score1 - m_score2;
+            if (minusResult == 1) {
+                return "Advantage player1";
+            } else if (minusResult == -1) {
+                return "Advantage player2";
+            } else if (minusResult >= 2) {
+                return "Win for player1";
+            } else {
+                return "Win for player2";
+            }
+        }
+
+        private String getEqualScore(int score) {
+            switch (score) {
+                case 0:
+                    return "Love-All";
+                case 1:
+                    return "Fifteen-All";
+                case 2:
+                    return "Thirty-All";
+                default:
+                    return "Deuce";
+            }
+        }
+
+        private String getScores(int score1, int score2) {
+            String score = "";
+            String[] scores = {"Love", "Fifteen", "Thirty", "Forty"};
+            score = scores[score1] + "-" + scores[score2];
+            return score;
         }
     }
-
-    private String getScores(int score1, int score2) {
-        String score = "";
-        String[] scores = {"Love", "Fifteen", "Thirty", "Forty"};
-        score = scores[score1] + "-" + scores[score2];
-        return score;
-    }
-} 
+}
