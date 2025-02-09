85246cd00458cdbddcc757e8b6d89a52f919628f
onepieceIsReal <onepiece.iiith@gmail.com>
2025-02-09 22:09:40 +0530

    Refactored code to address design smells

--- TennisGame.java
+++ TennisGame.java
@@ -1,76 +1,94 @@
-
 public class TennisGame1 implements TennisGame {
-    
-    private int m_score1 = 0;
-    private int m_score2 = 0;
+
+    private Score score;
     private String player1Name;
     private String player2Name;
 
     public TennisGame1(String player1Name, String player2Name) {
         this.player1Name = player1Name;
         this.player2Name = player2Name;
+        score = new Score();
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
-        else
-            m_score2 += 1;
+        if (playerName.equals("player1")) {
+            score.wonPoint(1);
+        } else {
+            score.wonPoint(2);
+        }
     }
 
     public String getScore() {
-        String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
+        return score.toString();
+    }
+
+    private static class Score {
+        private int player1Score = 0;
+        private int player2Score = 0;
+
+        public void wonPoint(int player) {
+            if (player == 1) {
+                player1Score += 1;
+            } else {
+                player2Score += 1;
+            }
+        }
+
+        @Override
+        public String toString() {
+            String score = "";
+            if (player1Score == player2Score) {
+                score = getScoreForTie(player1Score);
+            } else if (player1Score >= 4 || player2Score >= 4) {
+                score = getScoreForAdvantageOrWin(player1Score, player2Score);
+            } else {
+                score = getScoreForNormal(player1Score, player2Score);
+            }
+            return score;
+        }
+
+        private String getScoreForNormal(int player1Score, int player2Score) {
+            return scoreForPoint(player1Score) + "-" + scoreForPoint(player2Score);
+        }
+
+        private String scoreForPoint(int score) {
+            String scoreString = "";
+            switch (score) {
                 case 0:
-                        score = "Love-All";
+                    scoreString = "Love";
                     break;
                 case 1:
-                        score = "Fifteen-All";
+                    scoreString = "Fifteen";
                     break;
                 case 2:
-                        score = "Thirty-All";
+                    scoreString = "Thirty";
                     break;
+                case 3:
+                    scoreString = "Forty";
+                    break;
+            }
+            return scoreString;
+        }
+
+        private String getScoreForTie(int score) {
+            switch (score) {
+                case 0:
+                    return "Love-All";
+                case 1:
+                    return "Fifteen-All";
+                case 2:
+                    return "Thirty-All";
                 default:
-                        score = "Deuce";
-                    break;
-                
+                    return "Deuce";
             }
         }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+
+        private String getScoreForAdvantageOrWin(int player1Score, int player2Score) {
+            int minusResult = player1Score - player2Score;
+            if (minusResult == 1) return "Advantage player1";
+            else if (minusResult == -1) return "Advantage player2";
+            else if (minusResult >= 2) return "Win for player1";
+            else return "Win for player2";
         }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
-        }
-        return score;
     }
 }
