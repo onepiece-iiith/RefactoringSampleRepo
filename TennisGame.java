b10cc43fe011345e074fd711cf3c6980e52875ad
onepieceIsReal <onepiece.iiith@gmail.com>
2025-02-09 22:44:19 +0530

    Refactor: Decompose TennisGame class to improve maintainability

--- TennisGame.java
+++ TennisGame.java
@@ -1,76 +1,75 @@
-
 public class TennisGame1 implements TennisGame {
-    
     private int m_score1 = 0;
     private int m_score2 = 0;
     private String player1Name;
     private String player2Name;
+    private ScoreCalculator scoreCalculator;
 
     public TennisGame1(String player1Name, String player2Name) {
         this.player1Name = player1Name;
         this.player2Name = player2Name;
+        this.scoreCalculator = new ScoreCalculator();
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
+        if ("player1".equals(playerName)) {
             m_score1 += 1;
-        else
+        } else {
             m_score2 += 1;
+        }
     }
 
     public String getScore() {
+        return scoreCalculator.calculateScore(m_score1, m_score2);
+    }
+}
+
+class ScoreCalculator {
+    public String calculateScore(int m_score1, int m_score2) {
         String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
+        if (m_score1 == m_score2) {
+            score = getEqualScore(m_score1);
+        } else if (m_score1 >= 4 || m_score2 >= 4) {
+            score = getAdvantageOrWinScore(m_score1, m_score2);
+        } else {
+            score = getStandardScore(m_score1, m_score2);
         }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+        return score;
+    }
+
+    private String getEqualScore(int score) {
+        switch (score) {
+            case 0:
+                return "Love-All";
+            case 1:
+                return "Fifteen-All";
+            case 2:
+                return "Thirty-All";
+            default:
+                return "Deuce";
         }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
+    }
+
+    private String getAdvantageOrWinScore(int m_score1, int m_score2) {
+        int minusResult = m_score1 - m_score2;
+        if (minusResult == 1) {
+            return "Advantage player1";
+        } else if (minusResult == -1) {
+            return "Advantage player2";
+        } else if (minusResult >= 2) {
+            return "Win for player1";
+        } else {
+            return "Win for player2";
         }
+    }
+
+    private String getStandardScore(int m_score1, int m_score2) {
+        String score = "";
+        String[] scores = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
+
+        score = scores[m_score1];
+        score += "-";
+        score += scores[m_score2];
         return score;
     }
 }
