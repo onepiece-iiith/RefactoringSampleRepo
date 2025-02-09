e43044a17b2394875beae8f1082229f93c564ae8
onepieceIsReal <onepiece.iiith@gmail.com>
2025-02-09 18:00:28 +0530

    Refactored code for Switch Statement

--- TennisGame.java
+++ TennisGame.java
@@ -1,6 +1,4 @@
-
 public class TennisGame1 implements TennisGame {
-    
     private int m_score1 = 0;
     private int m_score2 = 0;
     private String player1Name;
@@ -19,57 +17,34 @@ public class TennisGame1 implements TennisGame {
     }
 
     public String getScore() {
+        if (m_score1 == m_score2) {
+            return getEqualScore();
+        } else if (m_score1 >= 4 || m_score2 >= 4) {
+            return getAdvantageOrWinScore();
+        } else {
+            return getStandardScore();
+        }
+    }
+
+    private String getEqualScore() {
+        String[] scoreStrings = {"Love-All", "Fifteen-All", "Thirty-All", "Deuce"};
+        return scoreStrings[Math.min(m_score1, 3)];
+    }
+
+    private String getAdvantageOrWinScore() {
+        int minusResult = m_score1 - m_score2;
+        if (minusResult == 1) return "Advantage player1";
+        else if (minusResult == -1) return "Advantage player2";
+        else if (minusResult >= 2) return "Win for player1";
+        else return "Win for player2";
+    }
+
+    private String getStandardScore() {
         String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
-        }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
+        String[] scoreStrings = {"Love", "Fifteen", "Thirty", "Forty"};
+        for (int i = 1; i < 3; i++) {
+            int tempScore = (i == 1) ? m_score1 : m_score2;
+            score += scoreStrings[Math.min(tempScore, 3)] + (i == 1 ? "-" : "");
         }
         return score;
     }
