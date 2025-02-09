a35428493199db3467063f3c41a6a47040733733
onepieceIsReal <onepiece.iiith@gmail.com>
2025-02-09 22:39:42 +0530

    Refactor: Decompose TennisGame class to improve maintainability

--- TennisGame.java
+++ TennisGame.java
@@ -3,6 +3,7 @@ public class TennisGame1 implements TennisGame {
     private int m_score2 = 0;
     private String player1Name;
     private String player2Name;
+    private ScoreCalculator scoreCalculator = new ScoreCalculator();
 
     public TennisGame1(String player1Name, String player2Name) {
         this.player1Name = player1Name;
@@ -17,31 +18,37 @@ public class TennisGame1 implements TennisGame {
     }
 
     public String getScore() {
-        if (m_score1 == m_score2) return getScoreForEvenScores();
-        if (m_score1 >= 4 || m_score2 >= 4) return getScoreForAdvantageOrWin();
-        return getScoreForUnevenScores();
+        return scoreCalculator.calculateScore(m_score1, m_score2);
+    }
+}
+
+class ScoreCalculator {
+    public String calculateScore(int m_score1, int m_score2) {
+        if (m_score1 == m_score2) return getScoreForEvenScores(m_score1);
+        if (m_score1 >= 4 || m_score2 >= 4) return getScoreForAdvantageOrWin(m_score1, m_score2);
+        return getScoreForUnevenScores(m_score1, m_score2);
     }
 
-    private String getScoreForEvenScores() {
-        String score = "";
-        switch (m_score1) {
+    private String getScoreForEvenScores(int score) {
+        String scoreString = "";
+        switch (score) {
             case 0:
-                score = "Love-All";
+                scoreString = "Love-All";
                 break;
             case 1:
-                score = "Fifteen-All";
+                scoreString = "Fifteen-All";
                 break;
             case 2:
-                score = "Thirty-All";
+                scoreString = "Thirty-All";
                 break;
             default:
-                score = "Deuce";
+                scoreString = "Deuce";
                 break;
         }
-        return score;
+        return scoreString;
     }
 
-    private String getScoreForAdvantageOrWin() {
+    private String getScoreForAdvantageOrWin(int m_score1, int m_score2) {
         String score = "";
         int minusResult = m_score1 - m_score2;
         if (minusResult == 1) score = "Advantage player1";
@@ -51,10 +58,11 @@ public class TennisGame1 implements TennisGame {
         return score;
     }
 
-    private String getScoreForUnevenScores() {
+    private String getScoreForUnevenScores(int m_score1, int m_score2) {
         String score = "";
         for (int i = 1; i < 3; i++) {
-            String playerScore = getScoreForSinglePlayer(i == 1 ? m_score1 : m_score2);
+            int tempScore = (i == 1 ? m_score1 : m_score2);
+            String playerScore = getScoreForSinglePlayer(tempScore);
             score += playerScore + (i == 1 ? "-" : "");
         }
         return score;
